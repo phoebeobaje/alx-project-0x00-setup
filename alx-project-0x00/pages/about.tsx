3df5d6b70574f772f39b0a5d3ed092ef1e4349dc
phoebeobaje <phoeberobertscareer@gmail.com>
2025-05-29 22:42:09 +0100

    adding about page and landing page

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,9 @@
+const About: React.FC = () => {
+return(
+    <div>
+        <h1 className="text-xl font-extralight text-center mt-20">About Page</h1>
+
+    </div>
+)
+}
+export default About;
---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,9 @@
+const Landing: React.FC = () => {
+return(
+    <div>
+        <h1 className="text-xl font-extralight text-center mt-20">Landing Page</h1>
+
+    </div>
+)
+}
+export default Landing;
